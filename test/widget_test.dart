3f8a335168e6e7516dd55f955e41cc3045946f0a
melkerm <melkerm@eelu.edu.eg>
2024-11-20 13:43:23 +0200

    remove unnessary code and init the code to start use the package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router_pack/app_start_point.dart';
 
 import 'package:go_router_pack/main.dart';
 
