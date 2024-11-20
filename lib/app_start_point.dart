26003aaa812133540642bf14970e8018bc8b4613
melkerm <melkerm@eelu.edu.eg>
2024-11-20 13:51:24 +0200

    add the router config and create 4 screen 1 signin and 3 for bottom nav bar

--- lib/app_start_point.dart
+++ lib/app_start_point.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_pack/screens/bottom_nav_bar_modules/home/home_screen.dart';
+import 'package:go_router_pack/screens/bottom_nav_bar_modules/profile/profile_screen.dart';
+import 'package:go_router_pack/screens/bottom_nav_bar_modules/setting/setting_screen.dart';
+import 'package:go_router_pack/screens/sigin_in/sigin_in_screen.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "Go Router",
+      routerConfig: _router,
     );
   }
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) {
+          return const SiginInScreen();
+        },
+      ),
+
+      GoRoute(
+        path: "/profile",
+        builder: (context, state) {
+          return const ProfileScreen();
+        },
+      ),
+
+      GoRoute(
+        path: "/home",
+        builder: (context, state) {
+          return const HomeScreen();
+        },
+      ),
+
+      GoRoute(
+        path: "/setting",
+        builder: (context, state) {
+          return const SettingScreen();
+        },
+      ),
+    ],
+  );
 }
